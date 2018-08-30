c2c70ae86e2085f57466c9d93daba30131189ad4
Brian Gillingham <bgilling@pitt.edu>
2018-08-30 12:18:32 -0400

    added Newspaper search box - if provided by the solution pack

--- templates/islandora-newspaper.tpl.php
+++ templates/islandora-newspaper.tpl.php
@@ -8,12 +8,18 @@
  * - $islandora_content: A rendered vertical tabbed newspapper issue browser.
  * - $parent_collections: An array containing parent collection IslandoraFedoraObject(s).
  * - $description: Rendered metadata descripton for the object.
+ * - $search_box: newspaper-specific search box html and javascript block.
  * - $metadata: Rendered metadata display for the binary object.
  *
  * @see template_preprocess_islandora_newspaper()
  */
 ?>
 <div class="islandora-newspaper-object islandora">
+<?php if ($search_box): ?>
+  <div id="newspaper_search_box">
+    <?php print $search_box; ?>
+  </div>
+<?php endif; ?>
   <div class="islandora-newspaper-content-wrapper clearfix">
     <?php if ($islandora_content_copy): ?>
       <div class="islandora-newspaper-content">
